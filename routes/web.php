467beae3fafed8a1a9b034b77af61a0874610f4d
nogomin <tttime0@gmail.com>
2019-08-11 19:59:34 +0900

    RESTful  라우터 추가

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-Route::get('/', function() {
-    return view('welcome');
-});
+//Route::get('/', 'WelcomeController@index');
+Route::resource('articles', 'ArticlesController');
 
 Route::get('/home', function() {
     return redirect(route('home'));
